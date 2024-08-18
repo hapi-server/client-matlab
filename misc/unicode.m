54e5f7be4aa396f4b04768ae5c12137aeb0615b2
Bob Weigel <rweigel@gmu.edu>
2024-08-17 22:22:38 -0400

    unicode note

--- misc/unicode.m
+++ misc/unicode.m
@@ -30,7 +30,7 @@ end
 if 1
 %%
 % Unicode data works on MATLAB 2018b and 2020a. However, the 4-byte
-% UTF-16 string appears as a space in these versions.
+% UTF-8 string appears as a space in these versions.
 server     = 'http://localhost:8999/TestData3.1/hapi';
 %server     = 'http://hapi-server.org/servers/TestData3.1/hapi';
 dataset    = 'dataset1';
@@ -46,4 +46,24 @@ meta
 data
 data.unicodevector{:}
 end
+
+if 1
+%%
+% Unicode data works on MATLAB 2018b and 2020a. However, the 4-byte
+% UTF-8 string appears as a space in these versions.
+server     = 'http://localhost:8999/TestData3.1/hapi';
+%server     = 'http://hapi-server.org/servers/TestData3.1/hapi';
+dataset    = 'dataset1';
+parameters = 'unicodescalar-1-byte-with-3-padding-nulls';
+start      = '1970-01-01T00:00:00.000Z';
+stop       = '1970-01-01T00:00:03.000Z';
+opts       = struct('logging',1,'usecache',0);
+
+meta = hapi(server, dataset, parameters)
+
+[data,meta] = hapi(server, dataset, parameters, start, stop, opts);
+meta
+data
+end
+
 
